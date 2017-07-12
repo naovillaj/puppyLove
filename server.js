ff37d98497108c5e9d1caeef3a7fa72e8e356225
Naomi Villanueva <naovillaj@gmail.com>
2017-07-12 13:23:06 -0500

    public2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use('/',express.static('assets'));
+app.use('/',express.static('public'));
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Iniciando en puerto 3000");
